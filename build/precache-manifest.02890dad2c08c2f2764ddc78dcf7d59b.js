fb76ecaae5094f4744fd9d14e52dcd840107d49d
achraf2020-code <achraf-saidi@outlook.com>
2022-08-08 03:33:02 +0200

    fix depencies

---
+++ build/precache-manifest.02890dad2c08c2f2764ddc78dcf7d59b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "46bab15d373719601e7c",
+    "url": "/snapWebSite/static/css/main.c853aab8.chunk.css"
+  },
+  {
+    "revision": "46bab15d373719601e7c",
+    "url": "/snapWebSite/static/js/main.46bab15d.chunk.js"
+  },
+  {
+    "revision": "45814764c26d8d71431a",
+    "url": "/snapWebSite/static/css/1.e22ac2a2.chunk.css"
+  },
+  {
+    "revision": "45814764c26d8d71431a",
+    "url": "/snapWebSite/static/js/1.45814764.chunk.js"
+  },
+  {
+    "revision": "b0e2f619331f4d0a91a1",
+    "url": "/snapWebSite/static/js/runtime~main.b0e2f619.js"
+  },
+  {
+    "revision": "8cad2ebb3d016b274141ec9f6e17c0f1",
+    "url": "/snapWebSite/static/media/icon-arrow-down.8cad2ebb.svg"
+  },
+  {
+    "revision": "9e6d3b2d1880027e288da1890d157b3b",
+    "url": "/snapWebSite/static/media/icon-arrow-up.9e6d3b2d.svg"
+  },
+  {
+    "revision": "276b2fcb07f9364e65590b33449b5bca",
+    "url": "/snapWebSite/static/media/client-audiophile.276b2fcb.svg"
+  },
+  {
+    "revision": "5a18f06cb371af930d310214c27feb63",
+    "url": "/snapWebSite/static/media/client-databiz.5a18f06c.svg"
+  },
+  {
+    "revision": "52f2652515bd4d5ff83459e5fa60fd22",
+    "url": "/snapWebSite/static/media/client-maker.52f26525.svg"
+  },
+  {
+    "revision": "58863135d87c4f78ffe0e8bf6e073470",
+    "url": "/snapWebSite/static/media/client-meet.58863135.svg"
+  },
+  {
+    "revision": "531ae8145a173f78d09f6c238a36eb09",
+    "url": "/snapWebSite/static/media/logo.531ae814.svg"
+  },
+  {
+    "revision": "60bb4d98b5aab2934bd6220a64c2f8f5",
+    "url": "/snapWebSite/static/media/icon-calendar.60bb4d98.svg"
+  },
+  {
+    "revision": "c5124739824111bfb4bd7dc30640e520",
+    "url": "/snapWebSite/static/media/icon-planning.c5124739.svg"
+  },
+  {
+    "revision": "aca0310c1e519e3818d119d142be601d",
+    "url": "/snapWebSite/static/media/icon-reminders.aca0310c.svg"
+  },
+  {
+    "revision": "630a69a8485b073b6f9ab979fec06756",
+    "url": "/snapWebSite/static/media/icon-todo.630a69a8.svg"
+  },
+  {
+    "revision": "bbcce338071d1f03db376b2316fdfaef",
+    "url": "/snapWebSite/static/media/icon-menu.bbcce338.svg"
+  },
+  {
+    "revision": "84f79f090c4e6d53cb3d4423af8b21b2",
+    "url": "/snapWebSite/static/media/icon-close-menu.84f79f09.svg"
+  },
+  {
+    "revision": "dcd083eb52eee971602abfa35296c217",
+    "url": "/snapWebSite/static/media/image-hero-mobile.dcd083eb.png"
+  },
+  {
+    "revision": "12751c5e883aacbfe3e5123637a19485",
+    "url": "/snapWebSite/static/media/image-hero-desktop.12751c5e.png"
+  },
+  {
+    "revision": "100099bd0ee95a268557ca06d239df36",
+    "url": "/snapWebSite/index.html"
+  }
+];
